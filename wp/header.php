e0b72b3d9e144f310c44010962176a59c8231dd1
joasyannick <joasyannick@gmail.com>
2024-06-14 19:46:20 +0200

    Header and Footer

---
+++ wp/header.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <link rel="profile" href="https://gmpg.org/xfn/11" />
+        <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+        <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
+        <?php wp_body_open(); ?>
