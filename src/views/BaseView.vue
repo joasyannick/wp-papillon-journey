353e0d48360c92c399d44bbc71acc36f9bd920e1
Joas Yannick Kinouani <joasyannick@gmail.com>
2025-01-20 19:23:48 +0000

    Create a Vue component for each application accessible from the phone

--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -2,8 +2,36 @@
   import { RouterView } from 'vue-router'
   import { onMounted, ref } from 'vue'
   import Two from 'two.js'
+  import PhoneIcon from '@/components/icons/Phone.vue'
+  import Apollon from '@/components/apps/apollon/Apollon.vue'
+  import Aurore from '@/components/apps/aurore/Aurore.vue'
+  import BelArgus from '@/components/apps/bel-argus/BelArgus.vue'
+  import Diane from '@/components/apps/diane/Diane.vue'
+  import Monarque from '@/components/apps/monarque/Monarque.vue'
+  import Vulcain from '@/components/apps/vulcain/Vulcain.vue'
+  import Settings from '@/components/apps/settings/Settings.vue'
 
   const game = ref< HTMLElement | null >( null )
+  const currentApp = ref<string | null>(null)
+  const apps = new Map( [
+      [ 'Apollon', { name: 'Apollon', component: Apollon } ],
+      [ 'Aurore', { name: 'Aurore', component: Aurore } ],
+      [ 'BelArgus', { name: 'BelArgus', component: BelArgus } ],
+      [ 'Diane', { name: 'Diane', component: Diane } ],
+      [ 'Monarque', { name: 'Monarque', component: Monarque } ],
+      [ 'Vulcain', { name: 'Vulcain', component: Vulcain } ],
+      [ 'Settings', { name: 'Settings', component: Settings } ]
+    ] )
+
+  const loadApp = ( key: string ) => {
+      if ( currentApp.value !== key) {
+        currentApp.value = key
+      }
+    }
+
+  const closeApp = () => {
+      currentApp.value = null
+    }
 
   onMounted( () => {
       const two = new Two( {
@@ -25,9 +53,14 @@
 
 <template>
   <main id="papj-main">
-    <article id="papj-phone-app">
+    <article id="papj-current-app" v-if="currentApp">
+      <component :is="apps.get( currentApp )?.component" @close="closeApp" />
     </article>
-    <aside id="papj-phone">
+    <aside id="papj-device">
+      <PhoneIcon />
+      <ul class="papj-apps">
+        <li v-for="[ key, app ] in apps" :key="key" :class="`papj-${ key }-launcher`" @click="loadApp( key )">{{ app.name }}</li>
+      </ul>
       <RouterView />
     </aside>
     <article id="papj-game" ref="game">
